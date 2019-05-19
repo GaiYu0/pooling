f0e145985445643c8c44e0d857a485ac40dae1dd
Yu Gai <yg1246@nyu.edu>
2019-05-19 13:38:01 +0800

    commit

--- data.py
+++ data.py
@@ -2,8 +2,31 @@ import networkx as nx
 import torch as th
 
 
-def barabasi_albert_graph_loader(n, m, device):
-    while True:
-        g = nx.barabasi_albert_graph(n, m)
-        adj = th.from_numpy(nx.adjacency_matrix(g).todense())
-        yield x, adj
+class DataLoader:
+    def __iter__(self):
+        return self
+
+    def next(self):
+        raise NotImplementedError()
+
+    @property
+    def n_feats():
+        raise NotImplementedError()
+
+
+class BarabasiAlbertGraphLoader(DataLoader):
+    def __init__(n, m, device):
+        self.n = n
+        self.m = m
+        self.device = device
+
+    def next(self):
+        while True:
+            g = nx.barabasi_albert_graph(n, m)
+            adj = th.from_numpy(nx.adjacency_matrix(g).todense()).to(self.device)
+            x = th.sum(adj, dim=1, keepdim=True)
+            yield x, adj
+
+    @property
+    def n_feats():
+        return 1
--- train.py
+++ train.py
@@ -7,22 +7,23 @@ import data
 
 parser = ArgumentParser()
 parser.add_argument('--data', type=str)
-parser.add_argument('--data-args', nargs='+', type=int)
+parser.add_argument('--data-args', type=int, nargs='+')
 parser.add_argument('--dbs', type=int, help='Discriminator Batch Size')
 parser.add_argument('--dlr', type=float, help='Discriminator Learning Rate')
 parser.add_argument('--glr', type=float, help='Generator Learning Rate')
 parser.add_argument('--gbs', type=int, help='Generator Batch Size')
 parser.add_argument('--gpu', type=int, help='GPU')
+parser.add_argument('--nf', type=int, nargs='+', help='Number of Features')
 parser.add_argument('--ni', type=int, help='Number of Iterations')
 args = parser.parse_args()
 
 device = th.device('cpu') if args.gpu < 0 else None  # TODO
-data_loader = getattr(data, args.data + '_loader')(*args.data_args, device=device)
+data_loader = getattr(data, args.data + 'Loader')(*args.data_args, device=device)
 discriminator = Discriminator().to(device)
-g_configs = [{'in_feats' : args.n_nidden,
-              'out_feats' : args.n_hidden,
-              'out_nodes' :
-              'aggregator' : 'mean'} for i in range(n_layers)]
+g_configs = [{'in_feats' : in_feats,
+              'out_feats' : out_feats,
+              'out_nodes' : out_nodes,
+              'aggregator' : 'mean'} for in_feats, out_feats, out_nodes in zip([data_loader.n_feats] + args.nf[:-1], args.nf[1:])]
 generator = Generator().to(device)
 
 d_optim = optim.Adam(discirminator.parameters(), args.dlr)
